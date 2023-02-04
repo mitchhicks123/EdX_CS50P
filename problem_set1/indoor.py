a430632f0b4e77481470bf7e7864d5724ca686e6
Mitch <Mitchell.Hicks1@ontariotechu.net>
2023-02-03 23:09:01 -0500

    cleaning main folder

--- problem_set1/indoor.py
+++
@@ -1,9 +0,0 @@
-
-#main function
-def main():
-  #Coverts user input from uppercase to lowercase if applicable and then prints output
-  userIn = input("What would you like to say? ").lower()
-  print(userIn)
-
-
-main()
