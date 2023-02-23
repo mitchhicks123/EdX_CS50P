452c607fc3a6294478bd87a402425e583e1731d2
Mitch <Mitchell.Hicks1@ontariotechu.net>
2023-02-22 23:11:38 -0500

    add chart using matplotlib

--- final_project/project.py
+++ final_project/project.py
@@ -1,15 +1,37 @@
 
 from stock import Stocks
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import yfinance as yf
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import sys
 
 
 def main():
     # get users input this is to test some of the code
     ticker = input("Ticker: ").strip().upper()
 
-    # pass values in to get data
+    # create a a stock object
     stock = Stocks(ticker)
     print(stock.print_all())
+    test = stock.revenue
+    test.reverse()
+    time_period = get_reporting_years(len(test))
+
+    plt.bar(time_period, test)
+    plt.xlabel('Revenue')
+    plt.ylabel('Amount in Billions')
+    plt.title('Amount over the past 5 years')
+    plt.show()
+    sys.exit()
+
+
+def get_reporting_years(count):
+    years = []
+    for i in range(count, 0, -1):
+        years.append((datetime.now() - relativedelta(years=i)).strftime('%Y'))
+    return years
 
 
 if __name__ == "__main__":
--- final_project/stock.py
+++ final_project/stock.py
@@ -13,11 +13,16 @@ class Stocks:
         self.ticker = ticker
         # overview
         self.pe, self.shares_out, self.market_cap = api.get_overview(ticker)
-        self.share_price = int(self.market_cap) / int(self.shares_out)
+        self.share_price = self.market_cap / self.shares_out
+
         # income statement
         self.revenue, self.gp, self.op, self.ni = api.get_income(ticker)
+
         # Cash Flow Statement
+        self.cash, self.capex = api.get_cash_flow(ticker)
+
         # Balance Sheet
+        self.equity = api.get_balance(ticker)
 
     def print_all(self):
-        return (f"{self.revenue} \n{self.gp} \n{self.op} \n{self.ni}\n-----\n{self.pe}\n{self.shares_out}\n{self.market_cap}\n{self.share_price}")
+        return (f"{self.revenue} \n{self.gp} \n{self.op} \n{self.ni}\n-----\n{self.pe}\n{self.shares_out}\n{self.market_cap}\n{self.share_price}\n-----\n{self.equity}\n------\n{self.capex}\n{self.cash}")
